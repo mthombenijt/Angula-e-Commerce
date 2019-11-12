8aef328b2df47158d7bdd3430cc13ad51f4096b1
mthombenijt <mthombenijt@yahoo.com>
2019-11-12 14:12:53 +0200

    second commit

--- src/app/Services/update.service.ts
+++ src/app/Services/update.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,11 @@ import { Injectable } from '@angular/core';
 export class UpdateService {
 
   constructor() { }
+
+  private prodCount = 0;
+  prodCountCountChange: Subject<number> = new Subject<number>();
+  UpdateCount(count: number) {
+      this.prodCount = count;
+      this.prodCountCountChange.next(this.prodCount);
+  }
 }
